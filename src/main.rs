5922483f55f6872cecde6c6855bceb656ee6c883
NWrenger <nils@wrenger.net>
2023-03-22 23:44:01 +0100

    :rocket: Implemeted an inplace version

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
 fn main() {
-    let array: Vec<i32> = vec![
+    let mut array: Vec<i32> = vec![
         -100, 200, 1, 6, 7, 5, 23, 9, 1000000000, 3232, 323, 4, 3, 45, 5, 456, -34, 43,
     ];
-    printlist(quicksort(array))
+    quicksort(&mut array);
+    println!("{:?}", array);
 }
 
-fn quicksort(a: Vec<i32>) -> Vec<i32> {
-    let pivot = if a.last().is_some() {
-        a[0]
+fn quicksort(a: &mut [i32]) {
+    let pivot = if let Some (&pivot) =  a.last() {
+        pivot
     } else {
-        return a;
+        return;
     };
-    let mut smaller = vec![];
-    let mut nor = vec![];
-    let mut bigger = vec![];
-    for x in a {
-        if x < pivot {
-            smaller.push(x)
-        }
-        if x > pivot {
-            bigger.push(x)
-        }
-        if x == pivot {
-            nor.push(x)
+    let mut index = 0;
+    for current in 0..a.len() - 1 {
+        if a[current] < pivot {
+            a.swap(index, current);
+            index += 1;
         }
     }
-    let mut b: Vec<i32> = vec![];
-    b.append(&mut quicksort(smaller));
-    b.append(&mut nor);
-    b.append(&mut quicksort(bigger));
-    b
-}
-
-fn printlist(list: Vec<i32>) {
-    for x in list {
-        print!("{x} ")
-    }
-    println!()
+    let last = a.len() - 1;
+    a.swap(index, last);
+    let (lower, upper) = a.split_at_mut(index);
+    quicksort(lower);
+    quicksort(&mut upper[1..]); // ignoring pivot element
 }
